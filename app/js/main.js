cb17303fbbbcbd6bd4f0d7d091af63a720eaf57c
vladlcobadsd <lcobadsd@gmail.com>
2023-07-05 19:31:55 +0200

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 $(function () {
 
   $('.reviews-slider__wrapper').slick({
+    infinite: false,
+
     prevArrow: '<button class="reviews-slider__arrow reviews-slider__arrow--prev"><svg width="20" height="20"><use xlink:href="images/sprite.svg#icon-left"></use></svg></button>',
 
     nextArrow: '<button class="reviews-slider__arrow reviews-slider__arrow--next"><svg width="20" height="20"><use xlink:href="images/sprite.svg#icon-left"></use></svg></button>',
@@ -8,6 +10,6 @@ $(function () {
     dots: true
 
   })
-
-  var mixer = mixitup('.popular-categories__wrapper');
 })
+
+var mixer = mixitup('.popular-categories__wrapper');
